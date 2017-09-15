475944a5908c61444316bae4679a716e18468524
gwendalmaxime <prince_imrahil@hotmail.fr>
2017-09-15 13:45:21 +0200

    Add files via upload

---
+++ soundplatform/index.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+$_SESSION['langue'] = "Français"; ?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title> </title>
+		<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="stylesheets/css/stylesheet.css">
+		<meta name="viewport" content="width=device-width" charset="UTF-8">
+		<lin href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"></lin>
+	</head>
+
+	<body>
+	<!-- #loader-wrapper--><!-- #loader -->
+		<div id="index-wrapper">
+			<div class="nav-wrapper">
+				<div class="row">
+					<div class="col col--2-of-12 col--m-1-of-5 col--s-1-of-4">
+						<div class="box">
+							<p>SAMPLE LOGO</p>
+						</div>
+					</div>
+					<div class="col col--1-of-12 col--push-7-of-12 col--m-1-of-5 col--s-1-of-4">
+						<div class="box">
+							<p>S'inscrire</p>
+						</div>
+					</div>
+					<div class="col col--1-of-12 col--m-1-of-5 col--s-1-of-4">
+						<div class="box">
+							<p>Se connecter</p>
+						</div>
+					</div>
+					<div class="col col--1-of-12 col--m-1-of-5 col--s-1-of-4">
+						<div class="box">
+							<p>A propos</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="welcome-text">BIENVENUE SUR <em>La Grange du Net</em><br>PLATEFORME DE MUSIQUE LIBRE
+				<div class="discover_button">
+					<p>Découvrir</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
